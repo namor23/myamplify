c25a83e1b5ca4b60017f7f6d9a4c233bbe5c7870
unknown <namor@post.bgu.ac.il>
2021-10-13 15:23:38 +0300

    remove HI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <b> HI </b>
+   
   
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
